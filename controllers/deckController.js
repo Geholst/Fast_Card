0e72208b440eb74a912e1d37ae42419a66f08f89
jberger2992 <jberger2992@gmail.com>
2023-05-08 17:30:39 -0400

    Deck controller work

--- controllers/deckController.js
+++ controllers/deckController.js
@@ -3,8 +3,21 @@ const router = express.Router();
 const {Deck, Flashcard, Profile, Box} = require('../models');
 //  api/deck
 
-// Get all Decks
-router.get("/")
+// Get all Decks from selected Profile
+router.get("/selectprofile/:id",(req,res)=>{
+    Deck.findAll({
+        where:{profile_id:req.params.id},
+        include:[Flashcard]
+    }).then(deck=>{
+        if(deck.length===0){
+            return res.status(404).json({msg:"No decks found for this profile."})
+        }
+        res.json(deck)
+    }).catch(err=>{
+        console.log(err);
+        res.status(500).json({msg:"ERROR",err})
+    })
+})
 
 
 module.exports = router;
