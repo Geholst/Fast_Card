5a26bfe5e5879971ca2bb4e9c7bd4fbfe816de60
jberger2992 <jberger2992@gmail.com>
2023-05-09 14:34:14 -0400

    login js work, will need to finish later

--- public/js/login.js
+++ public/js/login.js
@@ -6,25 +6,43 @@ loginForm.addEventListener("submit",e=>{
     e.preventDefault();
     const profileObj = {
         username:document.getElementById("login-username"),
-        firstName:document.getElementById("login-first-name"),
-        lastName:document.getElementById("login-last-name"),
-        email:document.getElementById("login-email"),
         password:document.getElementById("login-password")
     }
     fetch("/api/profile/login",{
         method:"POST",
         body:JSON.stringify(profileObj),
-        // --------------don't know after this
-        headers:{
-            "Content-Type":"application/json"
-        }
+        headers: {"Content-Type":"application/json"}
+
     }).then(res=>{
         if(res.ok){
+       //TODO: This needs to redirect properly
            location.href = "/"
         } else {
-            alert("trumpet sound")
+            alert(response.statusText)
         }
     })
 })
 
 // Signup a new Profile
+signupForm.addEventListener("submit",e=>{
+    e.preventDefault();
+    const profileObj = {
+        username:document.getElementById("signup-username"),
+        firstName:document.getElementById("signup-first-name"),
+        lastName:document.getElementById("signup-last-name"),
+        email:document.getElementById("signup-email"),
+        password:document.getElementById("signup-password")
+    }
+    fetch("/api/profile/",{
+        method:"POST",
+        body:JSON.stringify(profileObj),
+        headers: {"Content-Type":"application/json"}
+    }).then(res=>{
+        if(res.ok){
+        //TODO: This needs to redirect properly
+           location.href = "/"
+        } else {
+            alert(response.statusText)
+        }
+    })
+})
