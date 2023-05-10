8fa9055b79f77921935f1cc9f70cd9921cc1fd34
jberger2992 <jberger2992@gmail.com>
2023-05-10 14:18:56 -0400

    front end routes worked out

--- controllers/frontendRoutes.js
+++ controllers/frontendRoutes.js
@@ -1,16 +1,57 @@
-const express = require('express')
-const router = require("express").Router()
-const app = express()
+const express = require('express');
+const router = require("express").Router();
+const app = express();
+const {Profile, Deck, Flashcard} = require("../models");
 
-// respond with "hello world" when a GET request is made to the homepage
-router.get('/', (req, res) => {
-  res.render('main', {layout: 'index'})
-})
-
-
+// Route to render the login screen
 router.get("/login", async (req,res) => {
+  try {
+    if (req.session.userId) {
+      res.redirect("/dashboard")
+    } else {
       res.render("login", {layout: 'index'})
+    }  
+  } catch (err) {
+      console.log(err)
+      res.status(500).json({ msg: "ERROR", err });
+    }
 })
 
-  
-  module.exports = router
+// Route to render the dashboard screen
+router.get("/dashboard", async (req,res) => {
+  try {
+    if (!req.session.userId) {
+      res.redirect("/login")
+    }
+    const profileData = await Profile.findByPk(req.session.userId, {
+      include: {
+        all: true,
+        nested: true
+      }
+    })
+    const profile = profileData.get({plain: true})
+    console.log(profile)
+    return res.render("dashboard", profile)
+  } catch (err) {
+    console.log(err)
+    res.status(500).json({ msg: "ERROR", err });
+  }
+})
+
+// Route to render the review screen
+router.get("/review/:id", async (req,res) => {
+  try {
+    const flashcardsData = await Flashcard.findAll({
+      where:{DeckId:req.params.id}
+    })
+    const flashcards = flashcardsData.get({plain: true})
+    console.log(flashcards)
+    return res.render("review", flashcards)
+  } catch (err) {
+    console.log(err)
+    res.status(500).json({ msg: "ERROR", err });
+  }
+})
+
+
+module.exports = router
