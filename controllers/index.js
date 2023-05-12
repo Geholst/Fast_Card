a21829915bd5d1d748571311782d9bab37751d6e
Joel Perez <jxp489@gmail.com>
2023-05-11 17:27:34 -0700

    added date stamp and next review day stamp

--- controllers/index.js
+++ controllers/index.js
@@ -6,11 +6,13 @@ const deckRoutes = require("./deckController");
 const flashcardRoutes = require("./flashcardController");
 const frontendRoutes = require("./frontendRoutes");
 const sample = require("./sampleController");
+const reviewRoute = require("./reviewSessionController");
 
 router.use("/api/profile", profileRoutes);
 router.use("/api/deck", deckRoutes);
 router.use("/api/flashcard", flashcardRoutes);
 router.use("/sample", sample);
+router.use("/api/review", reviewRoute);
 router.use("/", frontendRoutes);
 
 module.exports = router;
