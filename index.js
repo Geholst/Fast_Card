b75a3260160d8f410d1c6cc2e3686aaa11a34fcf
Joel Perez <jxp489@gmail.com>
2023-05-13 11:32:45 -0700

    save

--- index.js
+++ index.js
@@ -33,8 +33,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(routes);
 
-app.use(routes);
-
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log("Now listening ", PORT));
 });
